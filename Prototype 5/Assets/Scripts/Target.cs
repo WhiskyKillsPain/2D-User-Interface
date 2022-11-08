44523ff09dc5f98bce9169549209df87deb6cb47
WhiskyKillsPain <111146078+WhiskyKillsPain@users.noreply.github.com>
2022-11-08 11:03:54 -0800

    Commit1

---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public int PointValue = 1;
+    public float MinSpeed = 10;
+    public float MaxSpeed = 20;
+    public float MaxTorque =10;
+
+    private Rigidbody2D _targetRb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _targetRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
